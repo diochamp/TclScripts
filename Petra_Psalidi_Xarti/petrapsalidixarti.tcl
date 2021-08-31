3f5098b982101adf29b1839a71a43ecbf9c1a658
Dionisis Chachampis <dionisishahabis@gmail.com>
2021-08-31 17:38:29 +0300

    Added three round. Gathering the results need change (not sure what i want to do!)

--- Petra_Psalidi_Xarti/petrapsalidixarti.tcl
+++ Petra_Psalidi_Xarti/petrapsalidixarti.tcl
@@ -4,38 +4,49 @@ source petrapsalidixarti_procs.tcl
 
 array set available_choices [petraPsalidiXartiArray]
 
+set rounds 1
+
 set number_of_plays 1
 
 array set Rules [createRulesArray]
 
 array set Results [createResultsArray]
 
-while {$number_of_plays <= 5} {
-  set user_choice [makeYourChoice available_choices]
+while {$rounds <=3} {
 
-  while True {
-    if {[validateUserInput available_choices $user_choice] == True} {
-      break
-    } else {
-      puts "Your choice is invalid, try again: "
-      set user_choice [makeYourChoice available_choices]
+  puts "Round: $rounds"
+
+  while {$number_of_plays <= 5} {
+    set user_choice [makeYourChoice available_choices]
+
+    while True {
+      if {[validateUserInput available_choices $user_choice] == True} {
+        break
+      } else {
+        puts "Your choice is invalid, try again: "
+        set user_choice [makeYourChoice available_choices]
+      }
     }
+
+    puts "You chose \"$available_choices($user_choice)\""
+
+    set tmp_list [getArrayValues available_choices]
+
+    set computer_choice [randomChoice $tmp_list]
+    puts "Computer chose \"$computer_choice\""
+
+    checkWhoWinsPerPlay available_choices Rules Results $user_choice $computer_choice $number_of_plays
+
+    parray Results
+
+    if {$number_of_plays == 5} {
+      findTheWinner Results
+      incr rounds
+      set number_of_plays 1
+      break
+    }
+
+    incr number_of_plays
   }
-
-  puts "You chose \"$available_choices($user_choice)\""
-
-  set tmp_list [getArrayValues available_choices]
-
-  set computer_choice [randomChoice $tmp_list]
-  puts "Computer chose \"$computer_choice\""
-
-  checkWhoWinsPerPlay available_choices Rules Results $user_choice $computer_choice $number_of_plays
-
-  parray Results
-
-  if {$number_of_plays == 5} {
-    findTheWinner Results
-  }
-
-  incr number_of_plays
+  
 }
